762f2fc433daa60c697d1fa97416f3f53667ffc7
gmork2 <gmork.02@gmail.com>
2020-03-24 19:55:20 +0100

    Created tests package

---
+++ tests/commands/script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import argparse
+import random
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # parser.add_argument('name')
+    # parser.add_argument('age')
+
+    args = parser.parse_args()
+    print("Hello")
