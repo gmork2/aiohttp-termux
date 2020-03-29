50886a489460e38fedc7b314543b30b18682ae38
gmork2 <gmork.02@gmail.com>
2020-03-29 22:34:33 +0200

    Added base class for all commands

---
+++ aiotermux/views.py
@@ -0,0 +1,17 @@
+import json
+import logging
+
+DEFAULT_METHODS = ('POST', 'GET')
+
+
+class BaseCommandView:
+    def __init__(self):
+        self.methods = {}
+
+        for method_name in DEFAULT_METHODS:
+            method = getattr(self, method_name.lower(), None)
+            if method:
+                self.register_method(method_name, method)
+
+    def register_method(self, method_name, method):
+        self.methods[method_name.upper()] = method
