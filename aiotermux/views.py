9e872d06b12898891ea926abf8b43ce7abf644cf
gmork2 <gmork.02@gmail.com>
2020-03-29 23:34:51 +0200

    Added dispatch async method to BaseCommandView

--- aiotermux/views.py
+++ aiotermux/views.py
@@ -1,6 +1,9 @@
 import json
 import logging
 
+from aiohttp.web_exceptions import HTTPMethodNotAllowed, HTTPBadRequest
+from aiohttp.web import Request
+
 DEFAULT_METHODS = ('POST', 'GET')
 
 
@@ -15,6 +18,17 @@ class BaseCommandView:
 
     def register_method(self, method_name, method):
         self.methods[method_name.upper()] = method
+
+    async def dispatch(self, request: Request):
+        method = self.methods.get(request.method.upper())
+        if not method:
+            raise HTTPMethodNotAllowed('', DEFAULT_METHODS)
+        path = request.match_info.copy()['path']
+        if not path:
+            raise HTTPBadRequest()
+        kw = await request.json()
+
+        return await method(*path.split('/'), **kw)
 
 
 class CommandView(BaseCommandView):
