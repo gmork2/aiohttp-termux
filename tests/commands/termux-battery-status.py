aa02360c6031e5933ef4f793a04174ead0d3282c
gmork2 <gmork.02@gmail.com>
2020-03-30 18:24:40 +0200

    Added battery status fake command

---
+++ tests/commands/termux-battery-status.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import argparse
+import json
+import sys
+
+# termux-battery-status -h
+# Usage: termux-battery-status
+# Get the status of the device battery.
+
+USAGE = """termux-battery-status
+Get the status of the device battery.
+"""
+
+DATA = {
+  "health": "GOOD",
+  "percentage": 100,
+  "plugged": "UNPLUGGED",
+  "status": "NOT_CHARGING",
+  "temperature": 25.0,
+  "current": 900000
+}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(USAGE)
+    args = parser.parse_args()
+    json_data = json.dumps(DATA)
+
+    sys.stdout.write(json_data)
