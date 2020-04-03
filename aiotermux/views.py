b31d50921c278a1a4fc6ae23bec1946f9b0233e0
gmork2 <gmork.02@gmail.com>
2020-04-03 19:37:09 +0200

    Set typing annotations in views

--- aiotermux/views.py
+++ aiotermux/views.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from typing import Callable
 
 from aiohttp.web_exceptions import HTTPMethodNotAllowed, HTTPBadRequest
 from aiohttp.web import Request
@@ -13,11 +14,11 @@ class BaseCommandView:
         self.methods = {}
 
         for method_name in DEFAULT_METHODS:
-            method = getattr(self, method_name.lower(), None)
+            method: Callable = getattr(self, method_name.lower(), None)
             if method:
                 self.register_method(method_name, method)
 
-    def register_method(self, method_name, method):
+    def register_method(self, method_name: str, method: Callable) -> None:
         self.methods[method_name.upper()] = method
 
     async def dispatch(self, request: Request):
@@ -33,17 +34,17 @@ class BaseCommandView:
 
 
 class CommandView(BaseCommandView):
-    def __init__(self, resource):
+    def __init__(self, resource: 'Resource'):
         super().__init__()
         self.resource = resource
 
 
 class Resource:
-    def __init__(self, prefix, *args):
+    def __init__(self, prefix: str, *args):
         self.prefix = prefix
         self.endpoint = CommandView(self)
 
-    def register(self, router: UrlDispatcher):
+    def register(self, router: UrlDispatcher) -> None:
         router.add_route('*', '/{prefix}/{{path:.*}}'.format(
             prefix=self.prefix), self.endpoint.dispatch)
 
