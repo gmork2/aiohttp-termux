d6e5d030484ade679736418b47462bbc9c2db02f
gmork2 <gmork.02@gmail.com>
2020-04-03 19:51:26 +0200

    Added make_command function

--- aiotermux/utils.py
+++ aiotermux/utils.py
@@ -2,6 +2,30 @@ import asyncio
 from typing import Tuple
 
 
+def make_command(*args, **kwargs) -> str:
+    """
+    >>> make_command('termux-dialog', confirm={'-i': 'text hint', '-t': 'Title'})
+    'termux-dialog confirm -i text hint -t Title'
+
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    head = [args[0]] if args else []
+    for a, b in kwargs.items():
+        head.append(a)
+        if isinstance(b, dict):
+            fragment = make_command(**b)
+        elif isinstance(b, list):
+            fragment = ' '.join(b)
+        else:
+            fragment = str(b)
+        head.append(fragment)
+    head.extend(str(s) for s in args[1:])
+
+    return ' '.join(head)
+
+
 async def run_command(cmd: str) -> Tuple[str, int]:
     """
     Run a shell command as subprocess.
